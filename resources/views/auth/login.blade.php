162afb849bfcc7b6fdc6b0ff5436671d9bb2f920
parthokar90 <parthokar90@gmail.com>
2026-06-01 22:12:31 +0600

    docker setup done with nginx configuration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,24 @@
 @extends('layouts.app')
 @section('title') Inventory | Login @endsection
+
 @section('content')
 <form method="POST" action="{{ route('login') }}">
     @csrf
+
     <div class="msg">Sign in to start your session</div>
+
     <div class="input-group">
         <span class="input-group-addon">
             <i class="material-icons">email</i>
         </span>
         <div class="form-line">
-            <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Email" value="{{ old('email') }}" required autofocus>
+            <input type="text"
+                class="form-control @error('email') is-invalid @enderror"
+                name="email"
+                placeholder="Email"
+                value="{{ old('email', 'admin@email.com') }}"
+                required autofocus>
+
             @error('email')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -17,12 +26,19 @@
             @enderror
         </div>
     </div>
+
     <div class="input-group">
         <span class="input-group-addon">
             <i class="material-icons">lock</i>
         </span>
         <div class="form-line">
-            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required>
+            <input type="password"
+                class="form-control @error('password') is-invalid @enderror"
+                name="password"
+                placeholder="Password"
+                value="12345678"
+                required>
+
             @error('password')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -30,14 +46,19 @@
             @enderror
         </div>
     </div>
+
     <div class="row">
         <div class="col-xs-8 p-t-5">
             <input type="checkbox" name="remember" id="remember" class="filled-in chk-col-pink">
             <label for="remember">Remember Me</label>
         </div>
+
         <div class="col-xs-4">
-            <button class="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
+            <button class="btn btn-block bg-pink waves-effect" type="submit">
+                SIGN IN
+            </button>
         </div>
     </div>
+
 </form>
 @endsection
